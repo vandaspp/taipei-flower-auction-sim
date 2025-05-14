6a57d5a148b04c3fdd437bd6bd0cdc7fb7bc4d3b
vandaspp <vandaspp1101@gmail.com>
2025-05-14 18:28:50 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,66 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
 
-app.use(express.static('public'));
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+let auctionData = {};
+let currentPrice = 0;
+let auctionInterval;
+let hasWinner = false;
+let participants = new Set();
+
+io.on('connection', socket => {
+  socket.on('preview', data => {
+    auctionData = data;
+    currentPrice = data.start;
+    hasWinner = false;
+    io.emit('preview', { item: data.item, start: data.start });
+  });
+
+  socket.on('start', data => {
+    auctionData = data;
+    currentPrice = data.start;
+    hasWinner = false;
+    io.emit('update', { item: data.item, price: currentPrice });
+    clearInterval(auctionInterval);
+    auctionInterval = setInterval(() => {
+      if (currentPrice - data.decrease >= data.min) {
+        currentPrice -= data.decrease;
+        io.emit('update', { item: data.item, price: currentPrice });
+      } else {
+        clearInterval(auctionInterval);
+        io.emit('stop');
+      }
+    }, data.interval);
+  });
+
+  socket.on('stop', () => {
+    clearInterval(auctionInterval);
+    io.emit('stop');
+  });
+
+  socket.on('bid', ({ user }) => {
+    if (!hasWinner) {
+      hasWinner = true;
+      clearInterval(auctionInterval);
+      io.emit('winner', { user, item: auctionData.item, price: currentPrice });
+    }
+  });
+
+  socket.on('join', ({ user }) => {
+    participants.add(user);
+    io.emit('userCount', participants.size);
+  });
+
+  socket.on('clear', () => {
+    io.emit('clear');
+  });
 });
