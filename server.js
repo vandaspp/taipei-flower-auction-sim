87dd04872cbc9d21d6bcd9dbdba252982a040585
vandaspp <vandaspp1101@gmail.com>
2025-05-14 18:44:36 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,59 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+let auctionState = {
+  item: '',
+  price: 0,
+  min: 0,
+  step: 0,
+  isRunning: false,
+  participants: new Set()
+};
+
+io.on('connection', socket => {
+  socket.on('join', ({ user }) => {
+    socket.username = user;
+    auctionState.participants.add(user);
+    io.emit('userCount', auctionState.participants.size);
+  });
+
+  socket.on('preview', data => {
+    auctionState = { ...auctionState, ...data, isRunning: false };
+    io.emit('preview', { item: data.item, price: data.price });
+  });
+
+  socket.on('start', () => {
+    auctionState.isRunning = true;
+    io.emit('start', auctionState);
+  });
+
+  socket.on('stop', () => {
+    auctionState.isRunning = false;
+    io.emit('stop');
+  });
+
+  socket.on('bid', () => {
+    if (auctionState.isRunning) {
+      auctionState.price -= auctionState.step;
+      if (auctionState.price < auctionState.min) auctionState.price = auctionState.min;
+      io.emit('priceUpdate', auctionState.price);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      auctionState.participants.delete(socket.username);
+      io.emit('userCount', auctionState.participants.size);
+    }
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Auction system running");
 });
